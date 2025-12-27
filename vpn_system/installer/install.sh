4ea131826a4d107e3c0e64b5791014cff7558aef
F4txhr <fattttoddd@gmail.com>
2025-12-27 23:45:46 +0000

    update/

--- vpn_system/installer/install.sh
+++ vpn_system/installer/install.sh
@@ -56,7 +56,7 @@ install_deps() {
             apt-get install -y python3 python3-pip python3-venv python3-psutil \
                 nginx certbot curl wget rsync socat cron jq vnstat fail2ban ufw
             ;;
-        centos|almalinux|rocky)
+        centos|almalinux|rocky|alinux)
             dnf install -y epel-release
             dnf install -y python3 python3-pip nginx certbot curl wget rsync socat cronie jq vnstat fail2ban ufw
             ;;
